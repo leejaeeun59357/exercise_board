2e8b5337ad4509a2b4dc5d4c3df3e4b96e630237
Leejaeeun <ne2227@naver.com>
2024-03-19 15:12:06 +0900

    [Base] 프로젝트 생성 및 README.md 파일 작성

---
+++ src/main/java/org/board/exercise_board/ExerciseBoardApplication.java
@@ -0,0 +1,13 @@
+package org.board.exercise_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/board/exercise_board/ExerciseBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.board.exercise_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
