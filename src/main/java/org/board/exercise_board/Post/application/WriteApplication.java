29f671a3f14830cd3108f6314da7482c813008c3
Leejaeeun <ne2227@naver.com>
2024-04-03 17:23:13 +0900

    [refactor] class 이름 변경

--- src/main/java/org/board/exercise_board/Post/application/WriteApplication.java
+++ src/main/java/org/board/exercise_board/Post/application/WriteApplication.java
@@ -5,14 +5,14 @@ import org.board.exercise_board.Post.domain.Dto.PostDto;
 import org.board.exercise_board.Post.domain.form.WriteForm;
 import org.board.exercise_board.Post.exception.PostCustomException;
 import org.board.exercise_board.Post.exception.PostErrorCode;
-import org.board.exercise_board.Post.service.WriteService;
+import org.board.exercise_board.Post.service.PostService;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class WriteApplication {
 
-  private final WriteService writeService;
+  private final PostService postService;
 
   /**
    * 제목과 내용이 null값으로 들어왔는지, 작성하고자 하는 사용자가 이메일 인증이 완료되었는지 검사 후 게시글 저장
@@ -33,10 +33,10 @@ public class WriteApplication {
     }
 
     // 이메일 인증이 완료되었는지 검사
-    if (!writeService.isEmailVerified(writerId)) {
+    if (!postService.isEmailVerified(writerId)) {
       throw new PostCustomException(PostErrorCode.NOT_VERIFIED_EMAIL);
     }
 
-    return writeService.savePost(writeForm, writerId);
+    return postService.savePost(writeForm, writerId);
   }
 }
