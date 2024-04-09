10fbe6066568700cc7476f4e94b2746b3f373d9b
Leejaeeun <ne222727@gmail>
2024-04-09 15:12:40 +0900

    [refactor] 사용되지 않는 메서드 및 import문 삭제

--- src/main/java/org/board/exercise_board/Post/domain/repository/PostRepository.java
+++ src/main/java/org/board/exercise_board/Post/domain/repository/PostRepository.java
@@ -1,8 +1,6 @@
 package org.board.exercise_board.Post.domain.repository;
 
-import java.util.List;
 import org.board.exercise_board.Post.domain.model.Post;
-import org.board.exercise_board.User.domain.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-  List<Post> findAllBySubjectAndUser(String subject, User user);
   Page<Post> findBySubjectContaining(String keyword, Pageable pageable);
 }
