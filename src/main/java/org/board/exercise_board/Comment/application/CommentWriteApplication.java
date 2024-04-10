0352f5a3524d162692d617b3ddace9a8ef070ce0
Leejaeeun <ne222727@gmail>
2024-04-10 20:20:49 +0900

    [feat] 댓글 작성 시 해당 게시글 작성자에게 알림 기능 구현
    - 게시글에 댓글이 작성되면 해당 게시글 작성자에게 실시간으로 알림이 가도록 함
    - 로그인 성공 시 SSE 연결

--- src/main/java/org/board/exercise_board/Comment/application/CommentWriteApplication.java
+++ src/main/java/org/board/exercise_board/Comment/application/CommentWriteApplication.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.board.exercise_board.Comment.domain.dto.CommentDto;
 import org.board.exercise_board.Comment.domain.form.CommentWriteForm;
 import org.board.exercise_board.Comment.service.CommentService;
+import org.board.exercise_board.Comment.service.NotificationService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommentWriteApplication {
 
   private final CommentService commentService;
+  private final NotificationService notificationService;
 
   @Transactional
   public CommentDto saveComment(CommentWriteForm commentWriteForm, Long postId, String writerId) {
+
+    notificationService.notifyComment(postId, writerId);
     return commentService.saveComment(commentWriteForm,postId,writerId);
   }
 
--- src/main/java/org/board/exercise_board/User/application/SignInApplication.java
+++ src/main/java/org/board/exercise_board/User/application/SignInApplication.java
@@ -1,9 +1,9 @@
 package org.board.exercise_board.User.application;
 
 import lombok.RequiredArgsConstructor;
+import org.board.exercise_board.Comment.service.NotificationService;
 import org.board.exercise_board.User.domain.Form.SignInForm;
 import org.board.exercise_board.User.domain.model.JwtToken;
-import org.board.exercise_board.User.domain.repository.UserRepository;
 import org.board.exercise_board.User.service.UserService;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +12,17 @@ import org.springframework.stereotype.Service;
 public class SignInApplication {
 
   private final UserService userService;
+  private final NotificationService notificationService;
 
+  /**
+   * 로그인하는 메서드, 로그인 완료되면 sse 연결을 시도한다.
+   *
+   * @param signInForm
+   * @return
+   */
   public JwtToken signin(SignInForm signInForm) {
-    return userService.signin(signInForm.getLoginId(), signInForm.getPassword());
+    JwtToken token = userService.signin(signInForm.getLoginId(), signInForm.getPassword());
+    notificationService.subscribe(signInForm.getLoginId());
+    return token;
   }
 }
