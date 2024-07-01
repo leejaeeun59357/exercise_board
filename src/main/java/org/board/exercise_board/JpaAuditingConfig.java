5be54c536e19ca40e4447647965b8831c278626b
Leejaeeun <ne222727@gmail.com>
2024-07-01 15:19:07 +0900

    [test] UserController 테스트 코드 작성
    - @WebMvcTest 사용하여 테스트 시 jpaAuditingHandler 에러 발생하여 @Configuration 분리

---
+++ src/main/java/org/board/exercise_board/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package org.board.exercise_board;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+
+}
